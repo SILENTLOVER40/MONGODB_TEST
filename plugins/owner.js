a6e907f8d542a751177c0f9fd384d275a1be340d
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-09 18:21:42 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,7 +1,5 @@
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
 const config = require('../config')
-
+const {cmd , commands} = require('../command')
 cmd({
     pattern: "owner",
     desc: "im owner",
