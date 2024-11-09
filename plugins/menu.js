1e68e9d9b25027d1d1feff3f4524e6f30a7c70c0
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-09 18:09:02 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const {readEnv} = require('../lib/database')
+const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
