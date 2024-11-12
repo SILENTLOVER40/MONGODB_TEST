53722739c3abb941d92aba689cb1d422adce7a76
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-12 09:40:19 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,35 +9,11 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let owner = ` 
-*HEAR IS BOT OWNER INFO*
-*NAME:-* *USMAN.S*
-*AGE:-* *20 YEARS*
-*PUBLIC NAME:-* *SILENTLOVER*
-
-> *BY SILENTLOVER432*
-`
-await conn.sendMessage(from, { text: owner ,
-  contextInfo: {
-    mentionedJid: [ '' ],
-    groupMentions: [],
-    forwardingScore: 999,
-    isForwarded: false,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: '120363232588171807@newsletter',
-      newsletterName: "SILENTLOVER432",
-      serverMessageId: 999
-    },
-externalAdReply: { 
-title: 'SILENTLOVER432',
-body: `${pushname}`,
-mediaType: 1,
-sourceUrl: "https://wa.me/+923096287432?text=HY SILENTLOVER4 I'M MSG YOU FROM OWNER ADS" ,
-thumbnailUrl: "https://files.catbox.moe/h5ddpq.jpg" ,
-renderLargerThumbnail: true,
-showAdAttribution: true
+m.replyContact = (name, info, number) => {
+		var vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 'FN:' + name + '\n' + 'ORG:' + info + ';\n' + 'TEL;type=CELL;type=VOICE;waid=' + number + ':+' + number + '\n' + 'END:VCARD'
+		conn.sendMessage(m.chat, { contacts: { displayName: SILENTLOVER, contacts: [{ vcard }] } }, { quoted: m })
 }
-}}, { quoted: mek})
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
