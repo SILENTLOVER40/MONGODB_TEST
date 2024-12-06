6685b886125dc93ab0db4d8dc6fcf87902882656
SILENTLOVER432 <newheroku00@gmail.com>
2024-12-06 13:23:03 +0500

    Update menu (1).js

--- plugins/menu (1).js
+++ plugins/menu (1).js
@@ -44,7 +44,7 @@ _*🌟 Reply with the Number you want to select*_
 
 > ${config.CAPTION}`;
 
-    const vv = await conn.sendMessage(from, { image: { url:config.ALIVE_IMG}, text: desc, 
+    const vv = await conn.sendMessage(from, { image: { url:config.ALIVE_IMG}, caption: desc, 
   contextInfo: {
     mentionedJid: [ '' ],
     groupMentions: [],
@@ -59,7 +59,8 @@ externalAdReply: {
 title: 'SILENT-SOBX-MD',
 body: `${pushname}`,
 mediaType: 1,
-sourceUrl: "https://whatsapp.com/channel/0029VaHO5B0G3R3cWkZN970s"
+sourceUrl: "https://whatsapp.com/channel/0029VaHO5B0G3R3cWkZN970s", 
+showAdAttribution: true
 }
 }}, { quoted: mek})
         conn.ev.on('messages.upsert', async (msgUpdate) => {
