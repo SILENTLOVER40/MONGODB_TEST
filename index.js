c32c331927fd9824a1954b9aec82796fab11aa41
SILENTLOVER432 <newheroku00@gmail.com>
2024-12-14 14:56:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -129,12 +129,16 @@ conn.ev.on('creds.update', saveCreds)
 //=============readstatus=======
 
 conn.ev.on('messages.upsert', async(mek) => {
-mek = mek.messages[0]
-if (!mek.message) return	
-mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])
-}
+  mek = mek.messages[0]
+  if (!mek.message) return
+  mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+  if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+    await conn.readMessages([mek.key])
+    const user = mek.key.participant
+    const text = `Your status seen just now by @${config.BOT_NAME}`
+    await conn.sendMessage(user, { text: text })
+    await conn.reaction(mek.key, '👀')
+  }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
