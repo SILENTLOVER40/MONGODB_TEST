3e5a40976d801fef8c67be40052e587e0ca9ede9
SILENTLOVER432 <newheroku00@gmail.com>
2024-12-15 18:10:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -327,9 +327,9 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
-
+}
 })
-}}
+}
 app.get("/", (req, res) => {
 res.send("HEY, SILENT-SOBX-MD STARTED ✅");
 });
